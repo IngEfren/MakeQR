21ac16fd50b2b5989d59d5e2a838969d1f0d521c
IngEfren <157951326+IngEfren@users.noreply.github.com>
2024-01-28 03:54:20 -0600

    Código básico
    Primer prueba pasando de texto a un código QR con Python

---
+++ Main.py
@@ -0,0 +1,14 @@
+# MODULOS PARA QUE EL CODIGO FUNCIONE
+# pip install qrcode
+
+# Importamos el modulo para trabajar con QRs
+import qrcode
+
+# Almacenamos el texto que queremos convertir a QR
+TextToQR = "Prueba de funcionamiento"
+
+# Generamos un codigo QR
+qr = qrcode.make(TextToQR)
+
+# Almacenamos la imagen del codigo QR
+qr.save("Codigo.png")
